6a2f8bc0fed33fab93a52d40d372998284d7727a
RomanieDelporte <romadelp@student.arteveldehs.be>
2021-01-13 21:03:51 +0100

    add search rest api && some new features + finish project

--- public/wp-content/themes/webshop/single-event.php
+++ public/wp-content/themes/webshop/single-event.php
@@ -8,7 +8,6 @@
                     <h2><?php the_title(); ?></h2>
                     <p><?php the_field('date'); ?></p>
                     <p><?php the_field('eventscontent'); ?></p>
-                    <h2>test</h2>
             </div>
     </div>
 </div>
