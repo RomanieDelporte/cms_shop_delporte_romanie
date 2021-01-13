67ecd81c12c97cf5eb6832b8d8549e6fd8941d98
RomanieDelporte <romadelp@student.arteveldehs.be>
2021-01-14 00:13:35 +0100

    add something

--- public/wp-content/themes/webshop/front-page.php
+++ public/wp-content/themes/webshop/front-page.php
@@ -24,6 +24,7 @@
                             </div>
                         <?php }
                         } ?>
+                        <h2>test</h2>
             </div>
         </div>
     </div>
